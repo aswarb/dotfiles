8d0d1fdaef5bdbb61f76421339b903680b3b6027
Andrew Swarbrick <andrewoswarbrick@hotmail.co.uk>
2026-02-01 15:09:03 +0000

    config changes

--- .config/eww/scripts/network_ip.sh
+++ .config/eww/scripts/network_ip.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ip_info=$(ip addr show | grep 'inet ' | grep -v '127.0.0.1' | head -1 | awk '{print $2}')
+ip_info=$(ip -4 addr show wlan0 | awk '/inet / {print $2}')
 echo "$ip_info"
